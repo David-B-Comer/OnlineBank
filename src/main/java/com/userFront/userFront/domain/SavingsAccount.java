9393dacb67a403da88464c38fa2dc0258f605e11
David-B-Comer <davec2233@gmail.com>
2021-08-29 16:31:42 -0400

    added SavingsAccount savingsTransactionList

--- src/main/java/com/userFront/userFront/domain/SavingsAccount.java
+++ src/main/java/com/userFront/userFront/domain/SavingsAccount.java
@@ -1,11 +1,14 @@
 package com.userFront.userFront.domain;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class SavingsAccount {
 
     private Long id;
     private int accountNumber;
     private BigDecimal accountBalance;
+
+    private List<SavingsTransaction> savingsTransactionList;
 
 }
