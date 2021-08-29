e8d1d31751b88a8d770bc8a75a818345ea0d0024
David-B-Comer <davec2233@gmail.com>
2021-08-29 16:58:43 -0400

    added SavingsTransaction setAmount

--- src/main/java/com/userFront/userFront/domain/SavingsTransaction.java
+++ src/main/java/com/userFront/userFront/domain/SavingsTransaction.java
@@ -70,4 +70,8 @@ public class SavingsTransaction {
     public double getAmount() {
         return amount;
     }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
