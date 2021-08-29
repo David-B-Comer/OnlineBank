9fa2022101b680161e4302be21760653a8b7a6ff
David-B-Comer <davec2233@gmail.com>
2021-08-29 16:32:50 -0400

    added SavingsAccount setAccountNumber

--- src/main/java/com/userFront/userFront/domain/SavingsAccount.java
+++ src/main/java/com/userFront/userFront/domain/SavingsAccount.java
@@ -22,4 +22,8 @@ public class SavingsAccount {
     public int getAccountNumber() {
         return accountNumber;
     }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 }
