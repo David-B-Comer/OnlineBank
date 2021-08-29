48b42ba15aa89d6910f376b8192995646b4c48af
David-B-Comer <davec2233@gmail.com>
2021-08-29 16:21:56 -0400

    added PrimaryAccount accountBalance

--- src/main/java/com/userFront/userFront/domain/PrimaryAccount.java
+++ src/main/java/com/userFront/userFront/domain/PrimaryAccount.java
@@ -1,8 +1,11 @@
 package com.userFront.userFront.domain;
 
+import java.math.BigDecimal;
+
 public class PrimaryAccount {
 
     private Long id;
     private int accountNumber;
+    private BigDecimal accountBalance;
 
 }
