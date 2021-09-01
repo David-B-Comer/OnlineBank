3d3956ec7340081208f28b4880a6eed949e6d717
David-B-Comer <davec2233@gmail.com>
2021-09-01 17:28:06 -0400

    added PrimaryTransaction annotations

--- src/main/java/com/userFront/userFront/domain/PrimaryTransaction.java
+++ src/main/java/com/userFront/userFront/domain/PrimaryTransaction.java
@@ -1,17 +1,25 @@
 package com.userFront.userFront.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
 public class PrimaryTransaction {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private Date date;
     private String description;
     private String type;
     private String status;
     private double amount;
     private BigDecimal availableBalance;
+
+    @ManyToOne
+    @JoinColumn(name = "primary_account_id")
     private PrimaryAccount primaryAccount;
 
     public PrimaryTransaction() {
