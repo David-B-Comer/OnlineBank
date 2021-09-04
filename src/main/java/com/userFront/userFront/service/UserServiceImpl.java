2f29e7e099b3f48d1ccb86443f415005200f97c4
David-B-Comer <davec2233@gmail.com>
2021-09-03 22:36:19 -0400

    added UserServiceImpl save

--- src/main/java/com/userFront/userFront/service/UserServiceImpl.java
+++ src/main/java/com/userFront/userFront/service/UserServiceImpl.java
@@ -1,7 +1,19 @@
 package com.userFront.userFront.service;
 
+import com.userFront.userFront.dao.UserDao;
+import com.userFront.userFront.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserDao userDao;
+
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+
 }
