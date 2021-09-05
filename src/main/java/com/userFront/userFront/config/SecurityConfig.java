664a3b74cd370fe7404ff6e7d756dda42d02ff08
David-B-Comer <davec2233@gmail.com>
2021-09-05 19:43:39 -0400

    added SecurityConfig passwordEncoder

--- src/main/java/com/userFront/userFront/config/SecurityConfig.java
+++ src/main/java/com/userFront/userFront/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.userFront.userFront.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.SecureRandom;
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +21,8 @@ public class SecurityConfig {
 
     private static final String SALT = "salt"; // Salt should be protected carefully
 
-
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }
 }
