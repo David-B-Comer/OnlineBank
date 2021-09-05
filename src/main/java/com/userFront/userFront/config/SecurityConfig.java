39734d70fce1d27b63cedccdb6a3dde6ba8b26b4
David-B-Comer <davec2233@gmail.com>
2021-09-05 19:40:07 -0400

    added SecurityConfig userSecurityService

--- src/main/java/com/userFront/userFront/config/SecurityConfig.java
+++ src/main/java/com/userFront/userFront/config/SecurityConfig.java
@@ -1,4 +1,21 @@
 package com.userFront.userFront.config;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired UserSecurityService userSecurityService;
+
+    private static final String SALT = "salt"; // Salt should be protected carefully
+
+
 }
