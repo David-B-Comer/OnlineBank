151a00884ec618666c0d19b6a2f94971b656a04e
David-B-Comer <davec2233@gmail.com>
2021-09-06 15:30:29 -0400

    added Authority getAuthority

--- src/main/java/com/userFront/userFront/domain/security/Authority.java
+++ src/main/java/com/userFront/userFront/domain/security/Authority.java
@@ -9,4 +9,10 @@ public class Authority implements GrantedAuthority {
     public Authority(String authority) {
         this.authority = authority;
     }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
 }
