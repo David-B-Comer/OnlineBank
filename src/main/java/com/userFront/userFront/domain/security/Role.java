cab53409d1cc1f6cf44383af54356bb7e6a491e7
David-B-Comer <davec2233@gmail.com>
2021-09-07 18:04:38 -0400

    added Role constructor

--- src/main/java/com/userFront/userFront/domain/security/Role.java
+++ src/main/java/com/userFront/userFront/domain/security/Role.java
@@ -1,4 +1,21 @@
 package com.userFront.userFront.domain.security;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int roleId;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<UserRole> userRoles = new HashSet<UserRole>();
+
+    public Role() {
+    }
 }
