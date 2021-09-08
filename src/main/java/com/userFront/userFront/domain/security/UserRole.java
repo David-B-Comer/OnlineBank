1fcecd3856742f28cd8595a8f06d535a3a68b2e0
David-B-Comer <davec2233@gmail.com>
2021-09-08 19:08:05 -0400

    added UserRole constructor

--- src/main/java/com/userFront/userFront/domain/security/UserRole.java
+++ src/main/java/com/userFront/userFront/domain/security/UserRole.java
@@ -1,4 +1,19 @@
 package com.userFront.userFront.domain.security;
 
+import com.userFront.userFront.domain.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_role")
 public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userRoleId;
+
+    public UserRole(User user, Role role) {
+        this.user = user;
+        this.role = role;
+    }
 }
