c47e2979a1aad1bbace576927901dc59613f1896
David-B-Comer <davec2233@gmail.com>
2021-09-10 19:59:04 -0400

    added UserSecurityService LOG

--- src/main/java/com/userFront/userFront/service/UserSecurityService.java
+++ src/main/java/com/userFront/userFront/service/UserSecurityService.java
@@ -1,4 +1,14 @@
 package com.userFront.userFront.service;
 
-public class UserSecurityService {
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
+
 }
