3a566b8871592ff290835be535c11d3fbbeded98
David-B-Comer <davec2233@gmail.com>
2021-09-10 20:00:01 -0400

    added UserSecurityService userDao

--- src/main/java/com/userFront/userFront/service/UserSecurityService.java
+++ src/main/java/com/userFront/userFront/service/UserSecurityService.java
@@ -1,6 +1,8 @@
 package com.userFront.userFront.service;
 
+import com.userFront.userFront.dao.UserDao;
 import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,8 @@ import java.util.logging.Logger;
 public class UserSecurityService implements UserDetailsService {
 
     private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
+
+    @Autowired
+    private UserDao userDao;
 
 }
