b814c069f3619b0a8eb73e770bf5d5254f0d2449
David-B-Comer <davec2233@gmail.com>
2021-09-13 19:10:01 -0400

    added AccountService createSavingsAccount

--- src/main/java/com/userFront/userFront/service/AccountService.java
+++ src/main/java/com/userFront/userFront/service/AccountService.java
@@ -1,8 +1,11 @@
 package com.userFront.userFront.service;
 
 import com.userFront.userFront.domain.PrimaryAccount;
+import com.userFront.userFront.domain.SavingsAccount;
 
 public interface AccountService {
 
     PrimaryAccount createPrimaryAccount();
+
+    SavingsAccount createSavingsAccount();
 }
