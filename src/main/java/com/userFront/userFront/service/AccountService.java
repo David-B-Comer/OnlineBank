679e8f9d01944d91aa799913b9acbd5227bc5fc4
David-B-Comer <davec2233@gmail.com>
2021-09-13 19:09:24 -0400

    added AccountService createPrimaryAccount

--- src/main/java/com/userFront/userFront/service/AccountService.java
+++ src/main/java/com/userFront/userFront/service/AccountService.java
@@ -1,4 +1,8 @@
 package com.userFront.userFront.service;
 
+import com.userFront.userFront.domain.PrimaryAccount;
+
 public interface AccountService {
+
+    PrimaryAccount createPrimaryAccount();
 }
