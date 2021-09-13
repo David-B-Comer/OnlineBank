0ad1618e5e575993946e8a352dc9f50268320de5
David-B-Comer <davec2233@gmail.com>
2021-09-12 20:49:38 -0400

    added UserServiceImpl createUser

--- src/main/java/com/userFront/userFront/service/UserServiceImpl.java
+++ src/main/java/com/userFront/userFront/service/UserServiceImpl.java
@@ -2,8 +2,12 @@ package com.userFront.userFront.service;
 
 import com.userFront.userFront.dao.UserDao;
 import com.userFront.userFront.domain.User;
+import com.userFront.userFront.domain.security.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -21,6 +25,30 @@ public class UserServiceImpl implements UserService{
 
     public User findByEmail(String email) {
         return userDao.findByEmail(email);
+    }
+
+    @Transactional
+    public User createUser(User user, Set<UserRole> userRoles) {
+
+        User localUser = userDao.findByUsername(user.getUsername());
+
+        if (localUser != null) {
+            LOG.info("User with username {} already exist. Nothing will be done. ", user.getUsername());
+        } else {
+            String encryptedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encryptedPassword);
+
+            for (UserRole ur : userRoles) {
+                roleDao.save(ur.getRole());
+            }
+
+            user.getUserRoles().addAll(userRoles);
+            user.setPrimaryAccount(accountService.createPrimaryAccount);
+            user.setSavingsAccount(accountService.createSavingsAccount);
+
+            localUser = userDao.save(user);
+        }
+        return localUser;
     }
 
 
