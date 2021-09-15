255ca74760c57c360cf2945b28fffc515efb3798
David-B-Comer <davec2233@gmail.com>
2021-09-14 20:19:49 -0400

    added AccountServiceImpl userService

--- src/main/java/com/userFront/userFront/service/UserServiceImpl/AccountServiceImpl.java
+++ src/main/java/com/userFront/userFront/service/UserServiceImpl/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package com.userFront.userFront.service.UserServiceImpl;
 
 import com.userFront.userFront.domain.PrimaryAccount;
 import com.userFront.userFront.service.AccountService;
+import com.userFront.userFront.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +14,7 @@ public class AccountServiceImpl implements AccountService {
 
     @Autowired
     private SavingsAccountDao savingsAccountDao;
+
+    @Autowired
+    private UserService userService;
 }
