74ba73fad8516c305ec9ed2f24bf06fc92fd7599
David-B-Comer <davec2233@gmail.com>
2021-09-16 22:00:16 -0400

    added PrimaryAccountDao findByAccountNumber

--- src/main/java/com/userFront/userFront/dao/PrimaryAccountDao.java
+++ src/main/java/com/userFront/userFront/dao/PrimaryAccountDao.java
@@ -1,4 +1,9 @@
 package com.userFront.userFront.dao;
 
-public interface PrimaryAccountDao {
+import com.userFront.userFront.domain.PrimaryAccount;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrimaryAccountDao extends CrudRepository<PrimaryAccount, Long> {
+
+    PrimaryAccount findByAccountNumber (int accountNumber);
 }
