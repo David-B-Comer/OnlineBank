42d9b0fb152fd3b07aaa5a40048de5944b785ec3
David-B-Comer <davec2233@gmail.com>
2021-09-16 22:04:07 -0400

    added SavingsAccountDao findByAccountNumber

--- src/main/java/com/userFront/userFront/dao/SavingsAccountDao.java
+++ src/main/java/com/userFront/userFront/dao/SavingsAccountDao.java
@@ -1,4 +1,9 @@
 package com.userFront.userFront.dao;
 
-public interface SavingsAccountDao {
+import com.userFront.userFront.domain.SavingsAccount;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SavingsAccountDao extends CrudRepository<SavingsAccount, Long> {
+
+    SavingsAccount findByAccountNumber(int accountNumber);
 }
