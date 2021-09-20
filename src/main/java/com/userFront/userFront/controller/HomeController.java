292b18e671cd8595ced5d61521a1831f1951ded4
David-B-Comer <davec2233@gmail.com>
2021-09-19 21:43:54 -0400

    added issue with thymeleaf templates

--- src/main/java/com/userFront/userFront/controller/HomeController.java
+++ src/main/java/com/userFront/userFront/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.HashSet;
 import java.util.Set;
 
+
 @Controller
 public class HomeController {
 
@@ -43,20 +44,22 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signupPost(@ModelAttribute("user") User user, Model model) {
+    public String signupPost(@ModelAttribute("user") User user,  Model model) {
 
         if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
 
             if (userService.checkEmailExists(user.getEmail())) {
                 model.addAttribute("emailExists", true);
             }
+
             if (userService.checkUsernameExists(user.getUsername())) {
                 model.addAttribute("usernameExists", true);
             }
+
             return "signup";
         } else {
             Set<UserRole> userRoles = new HashSet<>();
-            userRoles.add(new UserRole(user, roleDao.findByName("USER")));
+            userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
 
             userService.createUser(user, userRoles);
 
